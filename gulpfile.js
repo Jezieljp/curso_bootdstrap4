9693512777a21d438f96a79c21bae7244e35def0
jezieljp <cccccc157@hotmail.com>
2018-02-07 17:23:40 -0200

    Bootstrap 4

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+//chamando os plugins com require
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+//Compilar arquivo Sass
+gulp.task('sass', function () {
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss',
+        'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+//Move arquivos js para src/js
+gulp.task('js', function () {
+
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/popper.js/dist/umd/popper.js'])
+
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
+});
+
+// Servidor para atualizar tudo que for salvo no HTML E CSS
+gulp.task('serve', ['sass'], function () {
+    browserSync.init({
+        server: "./src"
+    });
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['js', 'serve']);
